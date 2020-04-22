8a2f7456dad8a295efb29185b5d10f42f05ef7c6
Leeloo <57699543+liberovao@users.noreply.github.com>
2020-04-22 09:11:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@
 def index(title=''):
     params = {'title': title}
     return render_template('base.html', **params)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    params = {'prof': prof, 'title': 'Тренинг'}
+    return render_template('training.html', **params)
