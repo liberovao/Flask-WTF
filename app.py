9020843fdfcbe346fb626668eb38e1a7fb29ff22
Leeloo <57699543+liberovao@users.noreply.github.com>
2020-04-22 09:21:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import flask, url_for, request, render_template
+import json
+
+app = Flask(__name__)
+
 @app.route('/index')
 def index(title=''):
     params = {'title': title}
@@ -8,3 +13,6 @@ def index(title=''):
 def training(prof):
     params = {'prof': prof, 'title': 'Тренинг'}
     return render_template('training.html', **params)
+
+if __name__ == '__main__':
+    app.run(port=80, host='127.0.0.1')
